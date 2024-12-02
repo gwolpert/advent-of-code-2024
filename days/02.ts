b21973ee920a4394d0e785e03c4fcdff886b3351
G Wolpert <ecj1fbwa@duck.com>
2024-12-02 17:14:59 +0100

    Day 02

--- days/02.ts
+++ days/02.ts
@@ -2,6 +2,12 @@ import { Solution } from '@types';
 import { count, enumerate, some, splitRows } from '@operators';
 import { every, filter, map, Observable as $, pairwise } from 'rxjs';
 
+const processInput = () => (source: $<string>) =>
+	source.pipe(
+		splitRows(),
+		map((row) => row.split(/\s+/).map(Number)),
+	);
+
 const validateReport = () => (source: $<number[]>) =>
 	source.pipe(
 		filter((numbers) =>
@@ -19,16 +25,14 @@ const validateReport = () => (source: $<number[]>) =>
 
 export const p1: Solution = (source) =>
 	source.pipe(
-		splitRows(),
-		map((row) => row.split(/\s+/).map(Number)),
+		processInput(),
 		validateReport(),
 		count(Boolean),
 	);
 
 export const p2: Solution = (source) =>
 	source.pipe(
-		splitRows(),
-		map((row) => row.split(/\s+/).map(Number)),
+		processInput(),
 		map((numbers) => [numbers, ...numbers.map((_, i) => numbers.filter((_, j) => i !== j))]),
 		some((report) => report.pipe(validateReport())),
 		count(),
